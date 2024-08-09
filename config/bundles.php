71e428d512edf73d0c4bda1072aaf46215308fa4
allistair <allistair.jak@gmail.com>
2024-08-09 07:39:31 +0200

    feat: create test environment
    add liip_fixtures_bundle for fixtures and test
    configure test database
    configure liip/test-fixtures-bundle in framework.yaml

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
